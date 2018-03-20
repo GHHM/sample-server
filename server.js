76690d9c10bdfe8f16cde06b80af164c54879251
Hyemi Kim <zozon1330@naver.com>
2018-03-20 14:14:38 +0900

    add middleware

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+//app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+  })
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
@@ -8,3 +15,5 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+
+
